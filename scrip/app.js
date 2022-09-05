5dd4ffd7f8e3aa8bb64e308f38997e0fefd2e3e8
Marcos <marcosreinoso2012@gmail.com>
2022-09-05 14:22:10 -0300

    ejercicios final 5/9

--- scrip/app.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-<script type="https://sm.correoargentino.com.ar/index.do?lang=">
-  function actualizar(){location.reload(true);}
-//Función para actualizar cada 4 segundos(4000 milisegundos)
-  setInterval("actualizar()",4000);
-</script>
-
-console.log('Hello world');
